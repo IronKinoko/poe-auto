5f068fefe177d8f2b65e6ec3683e8f699e0f0870
IronKinoko <kinoko_main@outlook.com>
2025-10-27 14:41:41 +0800

    feat: 添加conftest.py以强制添加项目根目录到sys.path

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,6 @@ import sys
 from pathlib import Path
 
 # 强制添加项目根目录
-project_root = Path(__file__).parent.resolve()
+project_root = Path(__file__).parent.parent.resolve()
 if str(project_root) not in sys.path:
     sys.path.insert(0, str(project_root))
